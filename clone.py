6df696b237f6d3c47863f78a19a609bbb400fd7a
Kotaro Hashimoto <hashimotokotaro@gmail.com>
2017-04-16 23:51:22 +0000

    added normalization layer

--- clone.py
+++ clone.py
@@ -20,19 +20,18 @@ for line in lines:
     measurement = float(line[3])
     measurements.append(measurement)
 
-print(images)
-
 X_train = np.array(images)
 y_train = np.array(measurements)    
 
 from keras.models import Sequential
-from keras.layers.core import Flatten, Dense
+from keras.layers.core import Flatten, Dense, Lambda
 
 model = Sequential()
-model.add(Flatten(input_shape = (160,320,3)))
+model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape = (160,320,3)))
+model.add(Flatten())
 model.add(Dense(1))
 
 model.compile(loss='mse',optimizer='adam')
-model.fit(X_train,y_train,validation_split=0.2,shuffle=True, nb_epoch=7)
+model.fit(X_train,y_train,validation_split=0.2,shuffle=True, nb_epoch=2)
 
 model.save('model.h5')
