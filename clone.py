a93a363902ced20b4cc8093f1db4d1abdd3e8062
Kotaro Hashimoto <hashimotokotaro@gmail.com>
2017-04-17 03:46:43 +0000

    LeNet5 is implemented

--- clone.py
+++ clone.py
@@ -24,14 +24,26 @@ X_train = np.array(images)
 y_train = np.array(measurements)    
 
 from keras.models import Sequential
-from keras.layers.core import Flatten, Dense, Lambda
+from keras.layers.core import Flatten, Dense, Lambda,Dropout
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape = (160,320,3)))
+model.add(Convolution2D(6, 5, 5, activation = 'relu', input_shape=(160, 320, 3)))
+model.add(Dropout(0.85))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6,5,5,activation = 'relu'))
+model.add(Dropout(0.85))
+model.add(MaxPooling2D())
+model.add(Dropout(0.85))
+model.add(MaxPooling2D())
 model.add(Flatten())
+model.add(Dense(128))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse',optimizer='adam')
-model.fit(X_train,y_train,validation_split=0.2,shuffle=True, nb_epoch=2)
+model.fit(X_train,y_train,validation_split=0.2,shuffle=True, nb_epoch=5)
 
 model.save('model.h5')
