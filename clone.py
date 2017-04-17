6fd2612f12efd5129c671e0070d4065e0ad80257
Kotaro Hashimoto <hashimotokotaro@gmail.com>
2017-04-17 04:11:10 +0000

    added augmented data

--- clone.py
+++ clone.py
@@ -20,8 +20,15 @@ for line in lines:
     measurement = float(line[3])
     measurements.append(measurement)
 
-X_train = np.array(images)
-y_train = np.array(measurements)    
+augmented_images, augmented_measurements = [],[]
+for image, measurement in zip(images, measurements):
+    augmented_images.append(image)
+    augmented_measurements.append(measurement)
+    augmented_images.append(cv2.flip(image,1))
+    augmented_measurements.append(measurement * -1.0)
+    
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)    
 
 from keras.models import Sequential
 from keras.layers.core import Flatten, Dense, Lambda,Dropout
@@ -34,8 +41,6 @@ model.add(Convolution2D(6, 5, 5, activation = 'relu', input_shape=(160, 320, 3))
 model.add(Dropout(0.85))
 model.add(MaxPooling2D())
 model.add(Convolution2D(6,5,5,activation = 'relu'))
-model.add(Dropout(0.85))
-model.add(MaxPooling2D())
 model.add(Dropout(0.85))
 model.add(MaxPooling2D())
 model.add(Flatten())
